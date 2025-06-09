baf994a1735c3e13ecabd1741558d1f2939b18dd
ihabTawffiq <37032155+ihabTawffiq@users.noreply.github.com>
2025-06-09 14:25:42 +0300

    adding hello world api

---
+++ src/main/java/com/example/alexITI/AlexItiApplication.java
@@ -0,0 +1,13 @@
+package com.example.alexITI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexItiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlexItiApplication.class, args);
+	}
+
+}
